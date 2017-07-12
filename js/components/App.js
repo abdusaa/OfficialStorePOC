9e1c027b29e4f9056cbbd0fba03d0b902896f6e6
Abdusa Alam <abdusa.alam@tokopedia.com>
2017-07-12 15:58:31 +0700

    add some code for triggering back to top component

--- js/components/App.js
+++ js/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import { ScrollView, View, Text, Button, StyleSheet, TouchableHighlight, Dimensions } from 'react-native'
+import { ScrollView, View, Text, Button, TouchableHighlight, Dimensions } from 'react-native'
+import OfficialStoreIntro from '../components/OfficialStoreIntro'
 import BannerContainer from '../containers/bannerContainer'
 import CampaignContainer from '../containers/campaignContainer'
 import BrandContainer from '../containers/brandContainer'
@@ -7,22 +8,39 @@ import Infographic from '../components/infographic'
 import BackToTop from '../common/BackToTop/backToTop'
 import Seo from '../components/seo'
 
+const { height } = Dimensions.get('window')
+const TRIGGER_BACK_TO_TOP = 60
+
 export default class App extends Component {
   onBackToTopTap = () => {
     this.refs.scrollView.scrollTo({ x: 0, y: 0, animatd: true });
   }
 
+  state = {
+    isVisible: false
+  }
+
+  scrollPos = (event) => {
+    this.setState({
+      isVisible: event.nativeEvent.contentOffset.y > TRIGGER_BACK_TO_TOP
+    })
+    //console.warn(this.state.isVisible)
+  }
+
+  //showScrollToTop = () => this.scrollPos() > TRIGGER_BACK_TO_TOP
+
   render() {
     return (
-      <View>
-        <ScrollView ref="scrollView">
-           <BannerContainer />
-          <CampaignContainer /> 
+      <View style={{backgroundColor: 'rgba(0, 0, 0, 0.05)'}}>
+        <ScrollView ref="scrollView" showsVerticalScrollIndicator={false} onScroll={this.scrollPos} >
+          <OfficialStoreIntro />
+          <BannerContainer />
+          <CampaignContainer />
           <BrandContainer />
            <Infographic />
           <Seo /> 
         </ScrollView>
-        <BackToTop onTap={this.onBackToTopTap} />
+       <BackToTop onTap={this.onBackToTopTap} isElVisible={this.state.isVisible} />
       </View>
     )
   }
